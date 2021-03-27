2137b80a230f720685d72099e700862021f1cc51
Alessandro Di Tecco <alessandro@ditecco.me>
2021-03-27 16:24:45 +0100

    Added generated models

---
+++ gen/models/index.ts
@@ -0,0 +1,7 @@
+export * from './model-error';
+export * from './new-notes';
+export * from './new-users-permissions-role';
+export * from './new-users-permissions-user';
+export * from './notes';
+export * from './users-permissions-role';
+export * from './users-permissions-user';
