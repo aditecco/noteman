372680d2394883367c51d45896b81798d47aa96f
Alessandro Di Tecco <alessandro@ditecco.me>
2021-04-01 02:00:13 +0200

    Updated Navbar styles

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -3,7 +3,6 @@ Navbar
 --------------------------------- */
 
 import styled from "styled-components";
-import { rem } from "../../../utils";
 
 export const Navbar = styled.nav.attrs({ className: "Navbar" })`
   position: fixed;
@@ -11,10 +10,10 @@ export const Navbar = styled.nav.attrs({ className: "Navbar" })`
   left: 0;
   right: 0;
   color: white;
-  background-color: rebeccapurple;
+  background-color: #fafafa;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  box-shadow: 0 2px 20px 6px rgba(0, 0, 0, 0.2);
+  box-shadow: 0 2px 6px 1px rgba(0, 0, 0, 0.1);
   padding: 6px 18px;
 `;
