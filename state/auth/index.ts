5c185559fb834394cda18480adfc783b0508b312
Alessandro Di Tecco <alessandro@ditecco.me>
2021-04-08 00:28:17 +0200

    Added loading cases

--- state/auth/index.ts
+++ state/auth/index.ts
@@ -5,6 +5,7 @@ auth slice
 import { createSlice } from "@reduxjs/toolkit";
 import { getUser, signInUser, signUpUser } from "./thunks";
 import { initialState } from "./initialState";
+import { getNotes } from "../notes/thunks";
 
 export const authSlice = createSlice({
   // ref: https://redux-toolkit.js.org/usage/immer-reducers#mutating-and-returning-state
@@ -29,6 +30,13 @@ export const authSlice = createSlice({
     // TODO pending & error cases
 
     /**
+     * signUpUser.pending
+     */
+    builder.addCase(signUpUser.pending, state => {
+      state.loading = "pending";
+    });
+
+    /**
      * signUpUser
      * @param state
      * @param action
@@ -36,8 +44,16 @@ export const authSlice = createSlice({
     builder.addCase(signUpUser.fulfilled, function (state, action) {
       return {
         ...state,
+        ...{ loading: "succeeded" },
         ...(action?.payload ?? {}),
       };
+    });
+
+    /**
+     * signInUser.pending
+     */
+    builder.addCase(signInUser.pending, state => {
+      state.loading = "pending";
     });
 
     /**
@@ -48,8 +64,16 @@ export const authSlice = createSlice({
     builder.addCase(signInUser.fulfilled, function (state, action) {
       return {
         ...state,
+        ...{ loading: "succeeded" },
         ...(action?.payload ?? {}),
       };
+    });
+
+    /**
+     * getUser.pending
+     */
+    builder.addCase(getUser.pending, state => {
+      state.loading = "pending";
     });
 
     /**
@@ -59,6 +83,7 @@ export const authSlice = createSlice({
      */
     builder.addCase(getUser.fulfilled, function (state, action) {
       state.user = action?.payload;
+      state.loading = "succeeded";
     });
   },
 });
