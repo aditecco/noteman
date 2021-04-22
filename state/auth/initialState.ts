de552888670d9c4fa759eeb442562ce9aace445f
Alessandro Di Tecco <alessandro@ditecco.me>
2021-04-23 01:35:39 +0200

    Removed persistent state

--- state/auth/initialState.ts
+++ state/auth/initialState.ts
@@ -3,17 +3,19 @@ initialState
 --------------------------------- */
 
 import { AuthState } from "../../types";
-import { TOKEN_STORAGE_KEY, USER_STORAGE_KEY } from "../../constants";
+// import { TOKEN_STORAGE_KEY, USER_STORAGE_KEY } from "../../constants";
 
 export const initialState: AuthState = {
   error: null,
   loading: "idle",
-  user:
-    typeof window !== "undefined"
-      ? JSON.parse(sessionStorage.getItem?.(USER_STORAGE_KEY))
-      : null,
-  jwt:
-    typeof window !== "undefined"
-      ? sessionStorage.getItem?.(TOKEN_STORAGE_KEY)
-      : null,
+  user: null,
+  jwt: null,
+  // user:
+  //   typeof window !== "undefined"
+  //     ? JSON.parse(sessionStorage.getItem?.(USER_STORAGE_KEY))
+  //     : null,
+  // jwt:
+  //   typeof window !== "undefined"
+  //     ? sessionStorage.getItem?.(TOKEN_STORAGE_KEY)
+  //     : null,
 };
